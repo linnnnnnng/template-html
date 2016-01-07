24429a1475537dba58c6739155de68aefd81b084
linnnnnnng <chinling.yeo@KULGCR-MBOP-142.local>
2016-01-07 12:29:46 +0800

    2016/1/7

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ $.address.init(function(event) {
  * All your scripts insert here
  * 
  */
+var isTablet, isMobile;
 $(function() {
 	//fancy box init
 	$('.fancybox').fancybox();
@@ -52,6 +53,13 @@ $(function() {
 	$('.scrollbar').mouseenter(function() {
 		$(".scrollbar").getNiceScroll().doScrollPos(0,0);
 	});
+	
+	isTablet = (/ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(navigator.userAgent.toLowerCase()));
+	
+	//mobile and tablet detection
+	if($.browser.mobile || isTablet){
+		//if is mobile or tablet
+	}
 });
 
 function goPage(sPage){
